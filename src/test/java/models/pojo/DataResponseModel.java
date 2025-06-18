02a1bc2ac810a4a0c0f91f191dc60f03c0154103
Artem <artem@gmail.com>
2025-06-18 20:07:20 +0300

    Initial commit

---
+++ src/test/java/models/pojo/DataResponseModel.java
@@ -0,0 +1,24 @@
+package models.pojo;
+
+public class DataResponseModel {
+
+    String name, updatedAt;
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,20 @@
+package tests;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    public static final String FREE_API_KEY_NAME = "x-api-key";
+    public static final String FREE_API_KEY_VALUE = "reqres-free-v1";
+    public static final String USERS_END_POINT = "/users/";
+    public static final String RESOURS_END_POINT = "/unknown/";
+
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+    }
+
+
+}

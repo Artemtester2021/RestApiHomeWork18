08e59aa04e5223812c43a100e86861576a22df66
Artem <artem@gmail.com>
2025-07-07 15:45:27 +0300

    Three change homeWork

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,14 +3,12 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
-import io.qameta.allure.restassured.AllureRestAssured;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 
 import java.util.Map;
 
@@ -23,23 +21,28 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = System.getProperty("screenResolution", "1920x1080");
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = String.format("https://%s:%s@%s/wd/hub",
-                System.getProperty("selenoid_login", "user1"),
-                System.getProperty("selenoid_password", "1234"),
-                System.getProperty("selenoid_host", "selenoid.autotests.cloud"));
+
+        boolean isRemoteRun = System.getProperty("selenoid_host") != null;
+
+        if (isRemoteRun) {
+            // Настройки для удалённого запуска
+            Configuration.remote = String.format("https://%s:%s@%s/wd/hub",
+                    System.getProperty("selenoid_login"),
+                    System.getProperty("selenoid_password"),
+                    System.getProperty("selenoid_host"));
+
+            Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+            Configuration.browserCapabilities = capabilities;
+        }
 
         RestAssured.baseURI = "https://demoqa.com";
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
-
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @BeforeEach
@@ -52,7 +55,11 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        Attach.addVideo();
+
+        if (System.getProperty("selenoid_host") != null) {
+            Attach.addVideo();
+        }
+
         closeWebDriver();
     }
 }
